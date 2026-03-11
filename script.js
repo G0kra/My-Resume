96181cfb205978f65499aaf2b802e864afbda4e1
Vinicis Gomes <97054816+G0kra@users.noreply.github.com>
2026-03-11 14:49:41 -0300

    Add links to GitHub and LinkedIn in projetos array

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const projetos = [
-    <a href="https://github.com/G0kra">Meu GitHub</a>,
+    <h2>Links</h2>
+    <a href="https://github.com/seuusuario" target="_blank"> GitHub
+    </a>
+    <br>
+    <a href="https://linkedin.com/in/seuusuario" target="_blank"> LinkedIn
+    </a>
     "App de Tarefas em JavaScript",
     "Calculadora Web"
 ];
-
 const lista = document.getElementById("projetos");
 
 projetos.forEach(projeto => {
